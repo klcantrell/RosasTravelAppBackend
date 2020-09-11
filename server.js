6d8aa3d127ecffcb48d38e41e331f19ecd986afe
Kalalau Cantrell <cantrellkalalau@gmail.com>
2020-09-10 20:18:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var app = require('express')(); // Create an instance of an Express app
+const app = require('express')();
 
-var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
+const mobileApp = require('azure-mobile-apps')();
 
-mobileApp.tables.add('Book'); // Create a table for 'Book' with default settings
+mobileApp.tables.add('Post');
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
