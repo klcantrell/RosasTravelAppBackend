ce91b59d3d98f9bf224f83551b10639af7f7b347
Kalalau Cantrell <cantrellkalalau@gmail.com>
2020-09-12 21:14:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = require('express')();
 const mobileApp = require('azure-mobile-apps')();
 
 mobileApp.tables.add('Post');
+mobileApp.tables.add('Users');
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
